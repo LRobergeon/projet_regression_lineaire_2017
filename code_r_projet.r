756f47da204357c4ab4e78daaa973d0a73524386
ppmdatix <paul.peseux@gmail.com>
2017-12-04 16:37:51 +0100

    mise_a_jour_code_deuxieme_partie

--- code_r_projet.r
+++ code_r_projet.r
@@ -14,7 +14,7 @@ train_data = read.table("data_groupe9.csv", header=TRUE,sep = ";")
 colonnes = colnames(train_data)
 stepall = length(colonnes)
 
-
+"""
 cd = c()
 pred_ok = c()
 for (x1 in 1:(stepall-3)){
@@ -43,7 +43,7 @@ for (x1 in 1:(stepall-3)){
     }
   }
 }
-
+"""
 
 liste_indice_a_enlever = c(1,12,34,45)
 
@@ -51,3 +51,64 @@ liste_indice_a_enlever = c(1,12,34,45)
 train_data_modified = train_data[ , - liste_indice_a_enlever]
 colonnes_modified = colnames(train_data_modified)
 stepall_modified = length(colonnes_modified)
+
+train_data_modified = data.frame(train_data_modified)
+essai = data.frame(train_data_modified[,12]) + data.frame(train_data_modified[,34])
+
+
+cd = c()
+
+for (x1 in 1:(stepall-3)){
+  for (x2 in (x1+1):(stepall-2)){
+    for (x3 in (x2+1):stepall-1){
+      X1 = cbind(train_data[,x1])
+      X2 = cbind(train_data[,x2])
+      X3 = cbind(train_data[,x3])
+
+      X1_2 = c()
+      for (x in X1){
+        X1_2 = c(X1_2, x*x)
+      }
+      X1_2 = cbind(X1_2)
+
+
+      X2_2 = c()
+      for (x in X2){
+        X2_2 = c(X2_2, x*x)
+      }
+      X2_2 = cbind(X2_2)
+
+
+      X3_2 = c()
+      for (x in X3){
+        X3_2 = c(X2_2, x*x)
+        }
+      X3_2 = cbind(X3_2)
+
+      X2X3 = c()
+	  for (i in 1:length(X2)) {
+	  	X2X3 = c(X2X3, X2[i,1]*X3[i,1])
+	  }
+	  X2X3 = cbind(X2X3)
+      
+      X2X1 = c()
+	  for (i in 1:length(X2)) {
+	  	X2X1 = c(X2X1, X2[i,1]*X1[i,1])
+	  }
+	  X2X1 = cbind(X2X1)
+
+	  X1X3 = c()
+	  for (i in 1:length(X2)) {
+	  	X1X3 = c(X1X3, X1[i,1]*X3[i,1])
+	  }
+	  X1X3 = cbind(X1X3)
+
+      reg = lm( X1 ~ X2 + X3 + X1_2 + X2_2 + X3_2 + X2X3 + X2X1 + X1X3)
+
+      coefficient_determination = summary(reg)$r.squared 
+      cd  = c(cd,coefficient_determination)
+	  }
+      
+    }}
+
+      
